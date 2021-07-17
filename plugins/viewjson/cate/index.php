d6a7eff0a384f2aa6ef9a657f5da26121be28bdc
unknown <dinhndvn@yahoo.com>
2021-07-17 10:05:53 +0700

    add viewjson

---
+++ plugins/viewjson/cate/index.php
@@ -0,0 +1,44 @@
+<?php
+	define('WP_USE_THEMES', false);
+	include_once $_SERVER['DOCUMENT_ROOT'].('/wp-load.php');
+	error_reporting(E_ALL ^ E_NOTICE);
+	
+	if(!$_GET['view']){
+		//Silent is Golden
+		exit();
+	}
+	
+	$theme_dir = '/wp-json/acf/v3/categories/';
+  $url = get_site_url();
+	$file = $url.$theme_dir.$_GET['view'];
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JSON VIEW</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="dist/jsonview.bundle.css" />
+</head>
+<body>
+  <div class="root"></div>
+
+  <script src="dist/jsonview.bundle.js"></script>
+  <script type="text/javascript">
+    fetch(<?php echo "'".$file."'"; ?>)
+    .then((res)=> {
+      return res.text();
+    })
+    .then((data) => {
+      const tree = JsonView.createTree(data);
+      JsonView.render(tree, document.querySelector('.root'));
+      JsonView.expandChildren(tree);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
+  </script>
+</body>
+</html>
