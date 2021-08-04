bfcf581a6bf84e8ee3c9cc38dc281b4b5778bc11
unknown <dinhndvn@yahoo.com>
2021-08-04 16:23:28 +0700

    fix execerpt

--- function_wp/remove_excerpt.php
+++ function_wp/remove_excerpt.php
@@ -22,3 +22,32 @@ function exc($param) {
 	}
     //return "Whew !".$param;
 }
+
+// Limit Excerpt Length by number of Words
+function excerpt( $limit ) {
+    $excerpt = explode(' ', get_the_excerpt(), $limit);
+    if (count($excerpt)>=$limit) {
+    array_pop($excerpt);
+    $excerpt = implode(" ",$excerpt).'';
+    } else {
+    $excerpt = implode(" ",$excerpt);
+    }
+    $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+    return $excerpt;
+}
+// echo excerpt(30);
+
+// Limit Content Length by number of Words
+function content($limit) {
+    $content = explode(' ', get_the_content(), $limit);
+    if (count($content)>=$limit) {
+    array_pop($content);
+    $content = implode(" ",$content).'...';
+    } else {
+    $content = implode(" ",$content);
+    }
+    $content = preg_replace('/[.+]/','', $content);
+    $content = apply_filters('the_content', $content);
+    $content = str_replace(']]>', ']]&gt;', $content);
+    return $content;
+}
