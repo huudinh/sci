ac53fc0dacd4265469b5d28f7553b8675e50abca
unknown <dinhndvn@yahoo.com>
2021-08-11 11:09:52 +0700

    add js

--- acf_module/module_basic.php
+++ acf_module/module_basic.php
@@ -7,4 +7,13 @@
     $path = $path.'/'.$link_module;
 
     include(locate_template("Module/Home/news_12_1_0/css/news_12_1_0_css.php")); 
+    
+    add_action( 'theme_js', 'seach_2_0_0_js' );
+
+    function seach_2_0_0_js(){
+        echo '
+           <script>
+           </script>
+        ';
+    }
 ?>
