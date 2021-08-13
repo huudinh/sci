56042e6bc490c0bb74c0a85d3f1a629decab8823
unknown <dinhndvn@yahoo.com>
2021-08-13 11:14:25 +0700

    add Filter Double Aray Item

--- javascript/filterSame.js
+++ javascript/filterSame.js
@@ -8,3 +8,48 @@ function filterSame(a) {
     }
     return result;
 }
+
+// Count phần tử trùng trong Array
+function countSame(arr) {
+    let arrNew = [];
+    for (let i = 0; i < arr.length; i++) {
+        let j = i + 1;
+        if (arr[i] == arr[j]) {
+            arrNew.push(arr[i]);
+            arrNew.push(arr[j]);
+        }
+    }
+    return arrNew.length;
+}
+
+// Filter Double Array Item
+function itemDouble(arr) {
+    let arrNew = [];
+    for (let i = 0; i < arr.length; i++) {
+        let j = i + 1;
+        if (arr[i] != arr[j]) {
+            arrNew.push(arr[i]);
+        }
+    }
+    return arrNew;
+}
+
+ // Count phần tử trùng trong Array
+function countSame2(arr) {
+    let arrClone = arr;
+    let arrNew = [];
+    for (let i = 0; i < arrClone.length; i++) {
+        count = 0;
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] == arrClone[i]) {
+                // arrNew.push(arr[j]);
+                count++
+            }
+        }
+        console.log(count + arrClone[i]);
+    }
+    // arrNew.push(count + arrClone[i]);
+    // return arrNew.length;
+    console.log(arrNew);
+}
+// countSame2(brandKNsv);
