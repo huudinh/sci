04972ecabe96e3175967ef6c2c248541fd17be22
unknown <dinhndvn@yahoo.com>
2021-09-20 15:07:50 +0700

    fix .html slug

--- javascript/slug_duplicate.js
+++ javascript/slug_duplicate.js
@@ -1,13 +1,15 @@
+//Fix duplicate url
 let str = window.location.href;
 strLast1 = str.charAt(str.length-1)
 strLast2 = str.charAt(str.length-2)
 strLast = strLast1 + strLast2;
-console.log(strLast1);
 
-if(strLast == '//'){
-    location.replace(str.slice(0, str.length-1));
-}
+if(str.indexOf(".html") == -1){
+    if(strLast == '//'){
+        location.replace(str.slice(0, str.length-1));
+    }
 
-if(strLast1 != '/'){
-    location.replace(str + '/');
+    if(strLast1 != '/'){
+        location.replace(str + '/');
+    }
 }
