c694e8206a6026aa82a802b4a44c0ac820840191
huudinh <dinhndvn@yahoo.com>
2021-11-20 10:41:18 +0700

    fix module basic

--- SCI_Theme/Module/module_sidebar.php
+++ SCI_Theme/Module/module_sidebar.php
@@ -5,25 +5,31 @@
     $categories = get_the_category();
 	$category_id = $categories[0]->cat_ID;
 
+    //Get Data Page Config
     $url = get_site_url().'/wp-json/acf/v3/pages/';
+    $apiUrl = $url.$module_id.'/group_page_field/';
+    $response = wp_remote_get($apiUrl);
+    $responseBody = wp_remote_retrieve_body( $response );
+    $page_json = json_decode( $responseBody );
+
+    //Get Data Cate
     $url_cate = get_site_url().'/wp-json/acf/v3/categories/'.$category_id;
-    $page_json = json_decode(file_get_contents($url.$module_id.'/group_page_field/'));
-
-    // Check sidebar
-    $sidebar_cate = json_decode(file_get_contents($url_cate));
-
+    $response_cate = wp_remote_get($url_cate);
+    $responseBody_cate = wp_remote_retrieve_body( $response_cate );
+    $sidebar_cate = json_decode( $responseBody_cate );
+    
+    //Check
     if (in_array('show_module_sidebar', $sidebar_cate->acf->group_admin)):
         $page_json = $sidebar_cate->acf;
     else:
         $page_json = $page_json;
     endif; 
     
-    // Process
+    // Loop Read Data
     foreach($page_json->group_page_field->sidebar_custom as $field): 
         if($field->acf_fc_layout == 'sidebar'): 
-            foreach($field->sidebar_sub_fields as $sidebar_data): 
-
-                $name = $sidebar_data->acf_fc_layout;
+            foreach($field->sidebar_sub_fields as $field_data): 
+                $name = $field_data->acf_fc_layout;
                
                 if(in_array($name,$arrcheck)){
                     $check = 1;
@@ -31,7 +37,9 @@
                     array_push($arrcheck,$name);
                     $check = 0;
                 }
-                include(locate_template('template-sidebar/content-'.$name.'.php'));	
+                if($name = 'sidebar_2_1_0'):
+                    include(locate_template('Module/Post/sidebar_2_1_0/sidebar_2_1_0.php'));	
+                endif;
             endforeach;
         endif;
     endforeach;
--- acf_module/module_basic.php
+++ acf_module/module_basic.php
@@ -1,10 +1,7 @@
 <?php
 // Khai bao css
-    $type = 'Home';
-    $name = 'news_12_3_0';
-    $link_module = 'Module/'.$type.'/'.$name.'/';
     $path = get_template_directory_uri();
-    $path = $path.'/'.$link_module;
+    $path = $path.'/Module/Home/news_12_1_0';
 
     include(locate_template("Module/Home/news_12_1_0/css/news_12_1_0_css.php")); 
     
