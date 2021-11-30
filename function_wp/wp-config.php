914a69b059d540d796d64bfc2c2843033ee1b009
huudinh <dinhndvn@yahoo.com>
2021-11-30 14:01:43 +0700

    fix wpcf

--- function_wp/wp-config.php
+++ function_wp/wp-config.php
@@ -1,3 +1,7 @@
 <?php
 //Khong cho edit theme
 define( 'DISALLOW_FILE_EDIT', true );
+
+//Disable updating everything for WordPress
+define( 'DISALLOW_FILE_MODS', true );
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
