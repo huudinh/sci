f72f5e47144761eb76dcf6f5b5787bac4240e7f4
huudinh <dinhndvn@yahoo.com>
2021-11-30 14:40:52 +0700

    wp cf

--- function_wp/wp-config.php
+++ function_wp/wp-config.php
@@ -5,3 +5,7 @@ define( 'DISALLOW_FILE_EDIT', true );
 //Disable updating everything for WordPress
 define( 'DISALLOW_FILE_MODS', true );
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
+
+//Limit Revisions
+define('AUTOSAVE_INTERVAL', 300); // seconds
+define('WP_POST_REVISIONS', 30);
