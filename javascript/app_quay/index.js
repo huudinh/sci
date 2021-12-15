cca797b852fd2d51c07ac5f000c86f421a51480e
Triệu Hữu Định <dinhth@Macbooks-iMac.local>
2021-12-15 15:47:07 +0700

    app_quay

--- javascript/app_quay/index.js
+++ javascript/app_quay/index.js
@@ -1,23 +1,82 @@
-//set cookies
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+let voucher = [{
+        name: 'Voucher 300K',
+        count: 35,
+    },
+    {
+        name: 'Voucher 500K',
+        count: 20,
+    },
+    {
+        name: 'Voucher 1 Triệu',
+        count: 5,
+    },
+    {
+        name: 'Voucher chăm sóc nha chu',
+        count: 20,
+    },
+    {
+        name: 'Voucher chăm sóc da',
+        count: 20,
+    },
+];
+
+let spin_wheel = (number) => {
+    let wheel = document.querySelector('#wheel .sec');
+    wheel.style.transform = `rotate(${number}deg)`;
 }
-//get cookies    
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+
+const spin = document.getElementById('spin');
+
+let count = 1;
+
+if (localStorage.getItem("checked")) {
+    spin.classList.add('spin-end');
+}
+
+spin.addEventListener('click', () => {
+    count++;
+    let checked = localStorage.getItem("checked");
+
+    if (count > 2 || checked) {
+        alert('Bạn đã hết lượt quay');
+        spin.classList.add('spin-end');
+        localStorage.setItem("checked", true);
+
+    } else {
+        let randnum = Math.floor((Math.random() * 100) + 1);
+
+        let number1 = voucher[0].count;
+        let number2 = number1 + voucher[1].count;
+        let number3 = number2 + voucher[2].count;
+        let number4 = number3 + voucher[3].count;
+        let number5 = number4 + voucher[4].count;
+
+        if (randnum <= number1) {
+            console.log(voucher[0].name);
+            spin_wheel(20149);
+            spin.classList.add('spin-end');
+
+        } else if (randnum <= number2) {
+            console.log(voucher[1].name);
+            spin_wheel(20239);
+            spin.classList.add('spin-end');
+
+        } else if (randnum <= number3) {
+            console.log(voucher[2].name);
+            spin_wheel(20261);
+            spin.classList.add('spin-end');
+
+        } else if (randnum <= number4) {
+            console.log(voucher[3].name);
+            spin_wheel(20284);
+            spin.classList.add('spin-end');
+
+        } else if (randnum <= number5) {
+            console.log(voucher[4].name);
+            spin_wheel(20306);
+            spin.classList.add('spin-end');
+
         }
     }
-    return "";
-}
+
+});
