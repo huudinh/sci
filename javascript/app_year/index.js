f2f5e481e3eee525c2690e15023f05d26aec6c07
huudinh <dinhndvn@yahoo.com>
2021-12-21 15:07:24 +0700

    fix

--- javascript/app_year/index.js
+++ javascript/app_year/index.js
@@ -1,11 +1,9 @@
 var yearSelect = document.querySelector('#year');
 
-(function populateYears() {
-    // get the current year as a number
+(function() {
     var date = new Date();
     var year = date.getFullYear();
 
-    // Make this year, and the 100 years before it available in the year <select>
     for (var i = 0; i <= 100; i++) {
         var option = document.createElement('option');
         option.textContent = year - i;
