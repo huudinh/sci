bf27996a2cea8ed6f94d418ef81fbd975b30d87f
huudinh <dinhndvn@yahoo.com>
2022-10-14 10:48:09 +0700

    update

--- php/remove_link.php
+++ php/remove_link.php
@@ -1,3 +1,7 @@
 <?php 
     $content = "text text text. <br><a href='http://www.example.com' target='_blank' title='title' style='text-decoration:none;'>name</a>";
     echo preg_replace("/<a[^>]+\>[a-z]+/i", "", $content);
+
+    // Remove string
+    $removeLink = '"text" text text';
+    echo str_replace('"', '', $removeLink);
