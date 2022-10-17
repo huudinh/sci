603924f26ced2a83dda050721e2dbbe065eaece3
huudinh <dinhndvn@yahoo.com>
2022-10-17 09:56:48 +0700

    update

---
+++ wp_quantri/quantri/index.php
@@ -0,0 +1,5 @@
+<?php
+$admin_cookie_code="babsdgsdgylongroup36dfdfgdfg5345345345fdsg2356fiashfjashfiuashf2010";
+setcookie("HackSessionTHD",$admin_cookie_code,0,"/");
+ header("Location:  /wp-login.php");
+?>
