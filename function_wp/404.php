aef9dce24d63864eba61069ca25d15cdf2cc3c0c
huudinh <dinhndvn@yahoo.com>
2022-10-19 11:59:19 +0700

    update

--- function_wp/404.php
+++ function_wp/404.php
@@ -1,12 +1,13 @@
+<!-- redirect html -->
+<meta http-equiv="refresh" content="0; url=https://benhvienthammykangnam.com.vn/kien-thuc-lam-dep/kangnam-khai-truong/?utm_source=youtube&utm_medium=ctw&utm_campaign=thuonghieu" />    
+<meta http-equiv="refresh" content="0;URL='<?php echo esc_url( home_url( '/' ) ); ?>'" /> 
+
 <?php
 // redirect trang chủ
     Header( "HTTP/1.1 301 Moved Permanently" );
     Header("Location: ".home_url()."");
 
 
-// redirect html
-<meta http-equiv="refresh" content="0; url=https://benhvienthammykangnam.com.vn/kien-thuc-lam-dep/kangnam-khai-truong/?utm_source=youtube&utm_medium=ctw&utm_campaign=thuonghieu" />    
-<meta http-equiv="refresh" content="0;URL='<?php echo esc_url( home_url( '/' ) ); ?>'" /> 
 
 // Redirect PHP
 $url = 'https://benhvienthammykangnam.com.vn';
