3753f6b4f4cea89add3ddaea5257f7a8b1adfd4e
huudinh <dinhndvn@yahoo.com>
2023-02-01 17:58:02 +0700

    update

--- wp_querry/post_basic.php
+++ wp_querry/post_basic.php
@@ -14,3 +14,8 @@ get_the_time('d/m/Y');
 
 // Get modified
 get_post_modified_time('d/m/Y');
+
+// Get slug
+$url      = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$url_path = parse_url( $url, PHP_URL_PATH );
+$post_slug = pathinfo( $url_path, PATHINFO_BASENAME );
