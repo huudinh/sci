f77765e5b848b94aa7f724bc23b734c5f1ac56be
huudinh <dinhndvn@yahoo.com>
2024-07-03 17:19:57 +0700

    update

--- function_wp/user_role.php
+++ function_wp/user_role.php
@@ -19,3 +19,18 @@ add_role('newbie', __(
         // 'edit_posts'      => true, // Allows user to edit their own posts
     )
 );
+
+
+// Khong cho tac gia xem bai viet cua nguoi khac trong wp-admin
+
+function posts_for_current_author($query) {
+    global $pagenow;
+    if( 'edit.php' != $pagenow || !$query->is_admin )
+    return $query;
+    if( !current_user_can( 'edit_others_posts' ) ) {
+        global $user_ID;
+        $query->set('author', $user_ID );
+    }
+    return $query;
+}
+add_filter('pre_get_posts', 'posts_for_current_author');
